eecbe8514c93a7540847389ca09f5a3d22274018
Ale Candia <alejandrogcandia@gmail.com>
2024-02-02 11:28:09 -0300

    se  agrega algo de Css a la card de horarios, y se crea componente Vista

--- src/redux/components/Children/Horarios/Card/Card.jsx
+++ src/redux/components/Children/Horarios/Card/Card.jsx
@@ -1,7 +1,7 @@
 import style from './Card.module.css'
 function Card ({data}){
     const {hora,id} = data;
-    return(<div key={id}>
+    return(<div key={id} className={`${style.btn} ${style.success}`}>
         <h2>{hora}</h2>
     </div>)
 }
---
+++ src/redux/components/Children/Vista/Vista.jsx
@@ -0,0 +1,7 @@
+import styles from './Vista.module.css'
+function Vista (){
+    return (<div>
+        <h1>Esta es la Vista</h1>
+    </div>)
+}
+export default  Vista;
