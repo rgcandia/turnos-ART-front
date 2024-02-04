45d89262200ce14ad668c54137ab54c560461f59
Ale Candia <alejandrogcandia@gmail.com>
2024-02-04 18:44:24 -0300

    load

--- src/components/Children/Horarios/Horarios.jsx
+++ src/components/Children/Horarios/Horarios.jsx
@@ -4,7 +4,7 @@ import Card from './Card/Card';
 function Horarios (){
     const {horas} =  useSelector(state=>state.data)
     return (<div className={style.horario}>
-        <h2>HORARIOS</h2>
+        <h2>Turnos</h2>
         <div className={style.vistaHora}>
             {horas&&horas.map(e=><Card datos={e} key={e.id}/>) }
            
