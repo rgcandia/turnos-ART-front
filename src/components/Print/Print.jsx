e13062232dfeede5535e4cb9b8a8d320dee3bd8a
Ale Candia <alejandrogcandia@gmail.com>
2024-02-09 14:32:50 -0300

    se crea un boton para la descarga de los datos en PDF, falta todavia centrar los datos, pero ya está funcional

---
+++ src/components/Print/Print.jsx
@@ -0,0 +1,40 @@
+import style from './Print.module.css'
+import autoTable from 'jspdf-autotable';
+import { IoPrintSharp } from "react-icons/io5";
+import jsPDF from 'jspdf';
+import { useSelector } from 'react-redux';
+
+function Print() {
+    const { users, horas } = useSelector(state => state.data);
+    const doc = new jsPDF();
+      // Configurar el estilo para el texto en todas las columnas
+      const columnTextStyle = { align: 'rigth' };
+    const handlePDF = () => {
+        
+        autoTable(doc, {
+            
+            head: [['Nombre Completo', 'Horario Reservado']],
+            body: [],
+            bodyStyles: {
+                cell: { halign: 'center', valign: 'middle' } // Estilos para todas las celdas del cuerpo
+            }
+        });
+
+        users.forEach((usuario) => {
+            const horaReservada = horas.find(hora => hora?.id === usuario.turno);
+            const horario = horaReservada ? horaReservada.hora : 'NO';
+            doc.autoTable({ body: [[usuario.name, horario]] });
+        });
+
+        doc.save('turnos.pdf');
+    };
+
+    return (
+        <div >
+            <button onClick={handlePDF} className={style.descargar}>
+                Descargar Turnos PDF
+            </button>
+        </div>
+    );
+}
+export default Print;
